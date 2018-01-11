16c01fa8b37a7be641c0b6a59eb728f11a51b32f
tikiatua <tikiatua@gmail.com>
2018-01-11 17:03:01 +0100

    add postcss configuration

---
+++ src/frontend/postcss.config.js
@@ -0,0 +1,15 @@
+// configuration for postcss to enable autoprefixer
+module.exports = {
+	plugins: [
+		require('autoprefixer')({
+			browsers: ['Chrome >= 60',
+				'Firefox >= 55',
+				'Edge >= 12',
+				'Explorer >= 10',
+				'Safari >= 10'],
+			cascade: true,
+			add: true,
+			remove: true
+		})
+	]
+};
